abd7c713c27edf7215abedc1cc264897f0105ef3
Lucas Cruz Wottrich <lucas.wottrich@ifood.com.br>
2021-12-14 15:49:00 -0300

    change sql order and change default value of is_selected to 0

--- database/src/main/java/wottrich/github/io/database/migration/DatabaseMigration.kt
+++ database/src/main/java/wottrich/github/io/database/migration/DatabaseMigration.kt
@@ -15,6 +15,6 @@ import wottrich.github.io.database.version.DatabaseVersions
  
 val MIGRATION_I_II = object : Migration(DatabaseVersions.I, DatabaseVersions.II) {
     override fun migrate(database: SupportSQLiteDatabase) {
-        database.execSQL("ALTER TABLE checklist ADD COLUMN is_selected INTEGER NOT NULL DEFAULT 0")
+        database.execSQL("ALTER TABLE checklist ADD COLUMN is_selected INTEGER DEFAULT 0 NOT NULL")
     }
 }
