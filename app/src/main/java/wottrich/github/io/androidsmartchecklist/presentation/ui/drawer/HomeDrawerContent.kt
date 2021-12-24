8a3eda7dd380689355456e66557f1cb261347373
Lucas Wottrich <wottrich78@gmail.com>
2021-12-23 21:30:22 -0300

    [Feature] Remove bottombar floating button and add button to new checklist in drawer

--- app/src/main/java/wottrich/github/io/androidsmartchecklist/presentation/ui/drawer/HomeDrawerContent.kt
+++ app/src/main/java/wottrich/github/io/androidsmartchecklist/presentation/ui/drawer/HomeDrawerContent.kt
@@ -1,21 +1,34 @@
 package wottrich.github.io.androidsmartchecklist.presentation.ui.drawer
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import org.koin.androidx.compose.getViewModel
+import wottrich.github.io.androidsmartchecklist.R
 import wottrich.github.io.androidsmartchecklist.presentation.viewmodel.HomeDrawerEffect
 import wottrich.github.io.androidsmartchecklist.presentation.viewmodel.HomeDrawerEvent
 import wottrich.github.io.androidsmartchecklist.presentation.viewmodel.HomeDrawerState
 import wottrich.github.io.androidsmartchecklist.presentation.viewmodel.HomeDrawerViewModel
+import wottrich.github.io.baseui.ui.Dimens
+import wottrich.github.io.baseui.ui.color.defaultButtonColors
 import wottrich.github.io.database.entity.ChecklistWithTasks
 
 /**
@@ -30,6 +43,7 @@ import wottrich.github.io.database.entity.ChecklistWithTasks
 @Composable
 fun HomeDrawerStatefulContent(
     onCloseDrawer: () -> Unit,
+    onAddNewChecklist: () -> Unit,
     viewModel: HomeDrawerViewModel = getViewModel()
 ) {
     val state by viewModel.drawerStateFlow.collectAsState()
@@ -44,7 +58,8 @@ fun HomeDrawerStatefulContent(
         state = state,
         onItemClick = {
             viewModel.processEvent(HomeDrawerEvent.ItemClicked(it))
-        }
+        },
+        onAddNewChecklist = onAddNewChecklist
     )
 
 }
@@ -52,13 +67,15 @@ fun HomeDrawerStatefulContent(
 @Composable
 private fun HomeDrawerStateless(
     state: HomeDrawerState,
-    onItemClick: (checklist: ChecklistWithTasks) -> Unit
+    onItemClick: (checklist: ChecklistWithTasks) -> Unit,
+    onAddNewChecklist: () -> Unit
 ) {
     when (state) {
         is HomeDrawerState.Loading -> CircularProgressIndicator()
         is HomeDrawerState.Content -> HomeDrawerSuccessContent(
             checklists = state.checklists,
-            onItemClick = onItemClick
+            onItemClick = onItemClick,
+            onAddNewChecklist = onAddNewChecklist
         )
     }
 }
@@ -66,20 +83,48 @@ private fun HomeDrawerStateless(
 @Composable
 private fun HomeDrawerSuccessContent(
     checklists: List<ChecklistWithTasks>,
-    onItemClick: (checklist: ChecklistWithTasks) -> Unit
+    onItemClick: (checklist: ChecklistWithTasks) -> Unit,
+    onAddNewChecklist: () -> Unit
 ) {
-    LazyColumn(content = {
-        items(checklists) { item ->
-            HomeDrawerChecklistItemComponent(checklist = item.checklist) {
-                onItemClick(item)
+    val buttonContentDescription = stringResource(id = R.string.floating_action_content_description)
+    Column(
+        modifier = Modifier.fillMaxHeight()
+    ) {
+        LazyColumn(
+            modifier = Modifier.weight(1f),
+            content = {
+                items(checklists) { item ->
+                    HomeDrawerChecklistItemComponent(checklist = item.checklist) {
+                        onItemClick(item)
+                    }
+                }
+                item {
+                    Spacer(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(56.dp)
+                    )
+                }
             }
-        }
-        item {
-            Spacer(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(56.dp)
+        )
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = Dimens.BaseFour.SizeTwo)
+                .semantics {
+                    contentDescription = buttonContentDescription
+                },
+            onClick = { onAddNewChecklist() },
+            colors = defaultButtonColors(),
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_round_add),
+                contentDescription = stringResource(
+                    id = R.string.floating_action_content_description
+                ),
+                tint = Color.White
             )
         }
-    })
+    }
+
 }
