757bb8e5811fff12a6616ece6a739677f9271f97
Lucas Wottrich <wottrich78@gmail.com>
2021-12-29 21:27:40 -0300

    [Feature] Add section to separate uncomplete and completed tasks

--- features/task/publicandroid/src/main/java/wottrich/github/io/publicandroid/presentation/ui/TaskLazyColumnComponent.kt
+++ features/task/publicandroid/src/main/java/wottrich/github/io/publicandroid/presentation/ui/TaskLazyColumnComponent.kt
@@ -1,16 +1,33 @@
 package wottrich.github.io.publicandroid.presentation.ui
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import wottrich.github.io.baseui.TextOneLine
 import wottrich.github.io.baseui.ui.Dimens
+import wottrich.github.io.baseui.ui.Dimens.BaseFour
 import wottrich.github.io.database.entity.Task
 
+private const val ANIMATION_DURATION = 200
+private val EnterAnimation = slideInHorizontally(animationSpec = tween(ANIMATION_DURATION))
+private val ExitAnimation = shrinkVertically(animationSpec = tween(ANIMATION_DURATION))
+
+@OptIn(ExperimentalFoundationApi::class, ExperimentalAnimationApi::class)
 @Composable
 fun TaskLazyColumnComponent(
     taskList: List<Task>,
@@ -18,20 +35,37 @@ fun TaskLazyColumnComponent(
     onDeleteTask: (Task) -> Unit,
     showDeleteItem: Boolean = true
 ) {
+    val hasCompletedList = taskList.any { it.isCompleted }
     LazyColumn(
         content = {
             items(taskList) { task ->
-
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
+                AnimatedVisibility(
+                    visible = !task.isCompleted,
+                    enter = EnterAnimation,
+                    exit = ExitAnimation
                 ) {
-                    Spacer(modifier = Modifier.height(Dimens.BaseFour.SizeTwo))
-                    TaskItemComponent(
+                    TaskItem(
                         task = task,
                         showDeleteItem = showDeleteItem,
-                        onCheckChange = { onCheckChange(task) },
-                        onDeleteTask = { onDeleteTask(task) }
+                        onCheckChange = onCheckChange,
+                        onDeleteTask = onDeleteTask
+                    )
+                }
+            }
+            if (hasCompletedList) {
+                completedHeader()
+            }
+            items(taskList) { task ->
+                AnimatedVisibility(
+                    visible = task.isCompleted,
+                    enter = EnterAnimation,
+                    exit = ExitAnimation
+                ) {
+                    TaskItem(
+                        task = task,
+                        showDeleteItem = showDeleteItem,
+                        onCheckChange = onCheckChange,
+                        onDeleteTask = onDeleteTask
                     )
                 }
             }
@@ -40,4 +74,40 @@ fun TaskLazyColumnComponent(
             }
         }
     )
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+private fun LazyListScope.completedHeader() {
+    stickyHeader {
+        Box(
+            modifier = Modifier.padding(all = BaseFour.SizeThree)
+        ) {
+            TextOneLine(
+                primary = {
+                    Text("Completed tasks")
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun TaskItem(
+    task: Task,
+    showDeleteItem: Boolean,
+    onCheckChange: (Task) -> Unit,
+    onDeleteTask: (Task) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Spacer(modifier = Modifier.height(Dimens.BaseFour.SizeTwo))
+        TaskItemComponent(
+            task = task,
+            showDeleteItem = showDeleteItem,
+            onCheckChange = { onCheckChange(task) },
+            onDeleteTask = { onDeleteTask(task) }
+        )
+    }
 }
