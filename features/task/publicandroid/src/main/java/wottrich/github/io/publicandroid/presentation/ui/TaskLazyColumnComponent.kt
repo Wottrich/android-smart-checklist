0f25d3de1bcfffbd0b1f7b8e51bc31c3f6e95f7a
Lucas Wottrich <wottrich78@gmail.com>
2022-03-16 20:25:01 -0300

    [Feature] swipe to delete task

--- features/task/publicandroid/src/main/java/wottrich/github/io/publicandroid/presentation/ui/TaskLazyColumnComponent.kt
+++ features/task/publicandroid/src/main/java/wottrich/github/io/publicandroid/presentation/ui/TaskLazyColumnComponent.kt
@@ -3,27 +3,40 @@ package wottrich.github.io.publicandroid.presentation.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.DismissDirection.EndToStart
 import androidx.compose.material.DismissDirection.StartToEnd
+import androidx.compose.material.DismissState
+import androidx.compose.material.DismissValue
+import androidx.compose.material.DismissValue.Default
 import androidx.compose.material.DismissValue.DismissedToStart
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FixedThreshold
+import androidx.compose.material.FractionalThreshold
 import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.Text
 import androidx.compose.material.rememberDismissState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import wottrich.github.io.baseui.TextOneLine
 import wottrich.github.io.baseui.icons.DeleteIcon
 import wottrich.github.io.baseui.ui.Dimens
@@ -58,51 +71,21 @@ fun TaskLazyColumnComponent(
             itemsIndexed(
                 items = list,
                 key = { _, item ->
-                    item.hashCode()
+                    item.taskId ?: item.hashCode()
                 },
                 itemContent = { _, task ->
-                    val state = rememberDismissState(
-                        confirmStateChange = {
-                            if (it == DismissedToStart) {
-                                onDeleteTask(task)
-                            }
-                            it == DismissedToStart
-                        }
-                    )
-
-                    SwipeToDismiss(
-                        state = state,
-                        background = {
-                            val color = when (state.dismissDirection) {
-                                StartToEnd -> Color.Transparent
-                                EndToStart -> SmartChecklistTheme.colors.status.negative
-                                else -> Color.Transparent
-                            }
-                            Box(
-                                modifier = Modifier
-                                    .fillMaxSize()
-                                    .background(color = color)
-                                    .padding(Dimens.BaseFour.SizeTwo)
-                            ) {
-                                if (color != Color.Transparent) {
-                                    DeleteIcon(
-                                        modifier = Modifier.align(Alignment.CenterEnd)
-                                    )
-                                }
-                            }
-                        },
-                        dismissContent = {
+                    SwipeToDeleteItem(
+                        onDeleteItem = { onDeleteTask(task) },
+                        onBackground = { state -> BackgroundSwipeToDeleteItem(state) },
+                        onDismissContent = {
                             TaskItem(
                                 task = task,
                                 showDeleteItem = showDeleteItem,
                                 onCheckChange = onCheckChange,
                                 onDeleteTask = onDeleteTask
                             )
-                        },
-                        directions = setOf(EndToStart)
+                        }
                     )
-
-
                 }
             )
             item {
@@ -112,8 +95,60 @@ fun TaskLazyColumnComponent(
     )
 }
 
+@ExperimentalMaterialApi
 @Composable
-fun TaskItem(
+private fun SwipeToDeleteItem(
+    onDeleteItem: () -> Unit,
+    onBackground: @Composable RowScope.(DismissState) -> Unit,
+    onDismissContent: @Composable RowScope.() -> Unit
+) {
+    val state = rememberDismissState(
+        confirmStateChange = {
+            val isDismissedToStart = it == DismissedToStart
+            if (isDismissedToStart) { onDeleteItem() }
+            true
+        }
+    )
+
+    SwipeToDismiss(
+        state = state,
+        background = { onBackground(state) },
+        dismissContent = onDismissContent,
+        directions = setOf(EndToStart)
+    )
+}
+
+@ExperimentalMaterialApi
+@Composable
+private fun BackgroundSwipeToDeleteItem(state: DismissState) {
+    val color = when (state.dismissDirection) {
+        StartToEnd -> Color.Transparent
+        EndToStart -> SmartChecklistTheme.colors.status.negative
+        else -> Color.Transparent
+    }
+    Box(
+        modifier = Modifier
+            .padding(top = BaseFour.SizeTwo)
+            .padding(horizontal = BaseFour.SizeThree)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(RoundedCornerShape(BaseFour.SizeTwo))
+                .background(color = color)
+                .padding(BaseFour.SizeTwo)
+        ) {
+            if (color != Color.Transparent) {
+                DeleteIcon(
+                    modifier = Modifier.align(Alignment.CenterEnd)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun TaskItem(
     task: Task,
     showDeleteItem: Boolean,
     onCheckChange: (Task) -> Unit,
