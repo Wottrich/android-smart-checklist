34d52ef52c2aee4b55646a346f053d8bcdf423a1
Lucas Cruz Wottrich <lucas.wottrich@ifood.com.br>
2022-03-24 20:38:50 -0300

    [Feature] Translate about us and help texts

--- app/src/main/java/wottrich/github/io/androidsmartchecklist/presentation/ui/drawer/HomeDrawerHelpAboutUseContent.kt
+++ app/src/main/java/wottrich/github/io/androidsmartchecklist/presentation/ui/drawer/HomeDrawerHelpAboutUseContent.kt
@@ -1,11 +1,14 @@
 package wottrich.github.io.androidsmartchecklist.presentation.ui.drawer
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import wottrich.github.io.androidsmartchecklist.R.string
 
 @Composable
 fun HelpAboutUsContent(
@@ -15,18 +18,27 @@ fun HelpAboutUsContent(
     Row(
         modifier = Modifier.fillMaxWidth()
     ) {
-        TextButton(
-            modifier = Modifier.weight(1f),
-            onClick = { onAboutUsClick() }
-        ) {
-            Text(text = "Sobre nós")
-        }
+        AboutUsTextButtonComponent(onAboutUsClick)
+        HelpTextButtonContent(onHelpClick)
+    }
+}
 
-        TextButton(
-            modifier = Modifier.weight(1f),
-            onClick = { onHelpClick() }
-        ) {
-            Text(text = "Ajuda")
-        }
+@Composable
+private fun RowScope.AboutUsTextButtonComponent(onAboutUsClick: () -> Unit) {
+    TextButton(
+        modifier = Modifier.weight(1f),
+        onClick = { onAboutUsClick() }
+    ) {
+        Text(text = stringResource(id = string.drawer_bottom_items_about_us))
+    }
+}
+
+@Composable
+private fun RowScope.HelpTextButtonContent(onHelpClick: () -> Unit) {
+    TextButton(
+        modifier = Modifier.weight(1f),
+        onClick = { onHelpClick() }
+    ) {
+        Text(text = stringResource(id = string.drawer_bottom_items_help))
     }
 }
