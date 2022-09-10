ebab2b2e5d680f47ee1cd50c8da275542f78d5c0
Lucas Cruz Wottrich <lucas.wottrich@ifood.com.br>
2022-09-10 10:38:43 -0300

    [Feature] Fix migration screen

--- app/src/main/java/wottrich/github/io/androidsmartchecklist/presentation/activity/MigrationActivity.kt
+++ app/src/main/java/wottrich/github/io/androidsmartchecklist/presentation/activity/MigrationActivity.kt
@@ -6,16 +6,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.flow.FlowCollector
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import wottrich.github.io.androidsmartchecklist.R
 import wottrich.github.io.androidsmartchecklist.presentation.viewmodel.MigrationUiEffect.MigrationSuccessful
 import wottrich.github.io.androidsmartchecklist.presentation.viewmodel.MigrationViewModel
+import wottrich.github.io.baseui.TextOneLine
 import wottrich.github.io.baseui.ui.ApplicationTheme
+import wottrich.github.io.baseui.ui.RowDefaults
+import wottrich.github.io.baseui.ui.TextStateComponent
 import wottrich.github.io.tools.extensions.startActivity
 
 @OptIn(InternalCoroutinesApi::class)
@@ -40,7 +44,15 @@ class MigrationActivity : AppCompatActivity() {
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.Center
                 ) {
-                    Text(text = "We are doing some adjustments...")
+                    TextOneLine(
+                        primary = {
+                            TextStateComponent(
+                                textState = RowDefaults.title(
+                                    text = stringResource(id = R.string.migration_loading)
+                                )
+                            )
+                        }
+                    )
                 }
             }
         }
