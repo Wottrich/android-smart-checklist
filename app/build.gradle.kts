19c7f625dcadf0ca031726143ce1f75a6cb9f9b7
Wottrich <lucas.wottrich@ifood.com.br>
2023-10-06 12:01:24 -0300

    [Refactor] Add BuildConfig as true on BuildFeature to generate build config on app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,7 @@ android {
 
     buildFeatures {
         compose = true
+        buildConfig = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
