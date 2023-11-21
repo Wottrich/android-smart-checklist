bc3867d3cd5031b3c26863813c70946ec1af442c
Wottrich <lucas.wottrich@ifood.com.br>
2023-11-21 18:22:57 -0300

    [Feature] Implement BaseTaskListItemType

--- features/task/impl/src/main/java/wottrich/github/io/smartchecklist/presentation/task/model/BaseTaskListItem.kt
+++ features/task/impl/src/main/java/wottrich/github/io/smartchecklist/presentation/task/model/BaseTaskListItem.kt
@@ -3,11 +3,15 @@ package wottrich.github.io.smartchecklist.presentation.task.model
 import androidx.annotation.StringRes
 import wottrich.github.io.smartchecklist.datasource.entity.NewTask
 
-sealed class BaseTaskListItem {
+enum class BaseTaskListItemType {
+    TASK, SECTION;
+}
+
+sealed class BaseTaskListItem(val type: BaseTaskListItemType) {
     fun getTaskItemOrNull(): TaskItem? = this as? TaskItem
 
-    data class TaskItem(val task: NewTask) : BaseTaskListItem()
+    data class TaskItem(val task: NewTask) : BaseTaskListItem(BaseTaskListItemType.TASK)
 
-    data class SectionItem(@StringRes val sectionName: Int) : BaseTaskListItem()
+    data class SectionItem(@StringRes val sectionName: Int) : BaseTaskListItem(BaseTaskListItemType.SECTION)
 
 }
