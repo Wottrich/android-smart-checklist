5ca674183c63efdf99d50385f5c8aa7869042980
Wottrich <lucas.wottrich@ifood.com.br>
2024-02-25 14:50:24 -0300

    Delete some wrong archives

--- features/quicklychecklist/impl/src/main/java/wottrich/github/io/quicklychecklist/impl/presentation/states/InitialQuicklyChecklistUiEffect.kt
+++
@@ -1,7 +0,0 @@
-package wottrich.github.io.quicklychecklist.impl.presentation.states
-
-sealed class InitialQuicklyChecklistUiEffect {
-    data class OnQuicklyChecklistJson(val quicklyChecklistJson: String) :
-        InitialQuicklyChecklistUiEffect()
-    object OnInvalidChecklist : InitialQuicklyChecklistUiEffect()
-}
